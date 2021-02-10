12e767e526b50dc230ac9abc0be60218c486c0ce
sherz <@mail.ru>
2021-02-10 23:35:14 +0600

    damn

--- day4/pytest_mathh.py
+++ day4/pytest_mathh.py
@@ -8,6 +8,9 @@ import mathh
 def test_add(num1, num2, res):
     assert mathh.add(num1, num2) == res
 
+def test_add():
+    assert mathh.mult(4,5) == 20
+
 @pytest.mark.add
 def test_addStrings():
     res = mathh.add('Hello','World')
@@ -17,3 +20,16 @@ def test_addStrings():
 def test_mult():
     res = mathh.mult(5,5)
     assert res == 25
+
+
+# import unittest
+# import mathh
+#
+# class TestMath(unittest.TestCase):
+#
+#     def test_add(self):
+#         self.assertEqual(mathh.add(5,7), 12)
+#         self.assertEqual(mathh.add(5,3), 8)
+#
+# if __name__ == '__main__':
+#     unittest.main()
